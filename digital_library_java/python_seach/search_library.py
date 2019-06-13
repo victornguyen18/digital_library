fc1761a0db90260b102497d91dfc28b416e05d0f
Victor Nguyen <thang181997@gmail.com>
2019-06-13 14:38:02 +0700

    - Update catch exception

--- digital_library_java/python_seach/search_library.py
+++ digital_library_java/python_seach/search_library.py
@@ -8,10 +8,10 @@ def get_search_book_google(search_term, num, page):
         google_book = gateway.entry_point.searchGoogleBook(search_term, num, page)
         google_book_obj = json.loads(google_book)
         return google_book_obj
-    except:
+    except Exception as e:
         google_book_obj = dict()
         google_book_obj["status"] = 404
-        google_book_obj["message"] = "Something wrong!! Please try again"
+        google_book_obj["message"] = "Something wrong!! Please try again." + str(e)
         google_book_obj["data"] = "[]"
         return google_book_obj
 
@@ -22,9 +22,9 @@ def get_search_book_ontology(search_term, num, page):
         books = gateway.entry_point.searchOntology(search_term, num, page)
         books_obj = json.loads(books)
         return books_obj
-    except:
+    except Exception as e:
         books_obj = dict()
         books_obj["status"] = 404
-        books_obj["message"] = "Something wrong!! Please try again"
+        books_obj["message"] = "Something wrong!! Please try again." + str(e)
         books_obj["data"] = {}
         return books_obj
