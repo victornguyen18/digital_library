958778395c17476816c300acd8c32028ec1eab25
Victor Nguyen <thang181997@gmail.com>
2019-07-22 18:11:52 +0700

    - Add hybrid rs

--- rs_system/recommender.py
+++ rs_system/recommender.py
@@ -14,6 +14,7 @@ logger = logging.getLogger('User similarity calculator')
 
 # Import Models
 from main_site.models import Rating, Similarity
+from title.models import Title
 import rs_system.builder.collaborative_filtering_calculator  as cl_calculator
 
 
@@ -91,7 +92,46 @@ def get_top_recs_using_collaborative_filtering(user_index, top_item=12, rating_d
     return list(cf_rating_df["title_id"])[:top_item]
 
 
+def process_rating_in_hybrid(sample):
+    cf_rating = sum(sample[sample.type == 'collaborative_filtering'].rating) * 0.7
+    cb_rating = sum(sample[sample.type == 'content_based'].rating) * 0.3
+    return pd.Series(dict(title_id=int(sample['title_id'].values[0]),
+                          rating=cf_rating + cb_rating))
+
+
+def get_top_recs_using_hybrid(user_index, top_item=12, rating_df=None):
+    if rating_df is None:
+        predicted_rating_df = pd.DataFrame(list(
+            Rating.objects.filter(user_id=user_index, type__in=['ib_predicted', 'ub_predicted']).order_by(
+                '-rating').values()))
+    else:
+        predicted_rating_df = rating_df[rating_df.user_id == user_index]
+    cf_rating_df = predicted_rating_df.groupby(by=['title_id']).apply(process).reset_index(drop=True)
+    cf_rating_df['type'] = "collaborative_filtering"
+    cb_suggestion_list = get_top_recs_using_content_based(user_index)
+    title_info_in_cb = Title.objects.filter(pk__in=cb_suggestion_list)
+    title_info_in_cb_df = pd.DataFrame(list(title_info_in_cb.values()))
+    hybrid_rs_list_df = pd.DataFrame()
+    hybrid_rs_list_df['title_id'] = title_info_in_cb_df['id']
+    hybrid_rs_list_df['rating'] = title_info_in_cb_df['rating']
+    hybrid_rs_list_df['type'] = "content_based"
+    hybrid_rs_list_df = hybrid_rs_list_df.append(cf_rating_df, ignore_index=True, )
+    hybrid_rs_list_df['rating'] = pd.to_numeric(hybrid_rs_list_df['rating'], errors='coerce')
+    result_hybrid_rs_list_df = hybrid_rs_list_df.groupby(by=['title_id']) \
+        .apply(process_rating_in_hybrid) \
+        .reset_index(drop=True)
+    result_hybrid_rs_list_df['title_id'] = pd.to_numeric(result_hybrid_rs_list_df['title_id'],
+                                                         downcast='integer',
+                                                         errors='coerce')
+    print(result_hybrid_rs_list_df.dtypes)
+    result_hybrid_rs_list_df = result_hybrid_rs_list_df.sort_values(by=['rating'], ascending=False).reset_index(
+        drop=True)
+    return list(result_hybrid_rs_list_df["title_id"])[:top_item]
+
+
 if __name__ == '__main__':
     user_id = 5
-    res_nb = cl_calculator.CollaborativeFiltering()
-    print(get_top_recs_using_collaborative_filtering(5, rating_df=res_nb.predict_all_item()))
+    rs_list = get_top_recs_using_hybrid(user_index=user_id)
+    print(rs_list)
+    # res_nb = cl_calculator.CollaborativeFiltering()
+    # print(get_top_recs_using_collaborative_filtering(5, rating_df=res_nb.predict_all_item()))
