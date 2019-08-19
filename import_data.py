e0b9d032c3fa9ff02d50253ddd51c104f38413c7
Victor Nguyen <thang181997@gmail.com>
2019-08-19 13:21:53 +0700

    Add encode  in read file

--- import_data.py
+++ import_data.py
@@ -14,28 +14,28 @@ cur = connection.cursor()
 
 logger.info("Import group, user, user_group")
 filename = 'databases/auth_import.sql'
-file = open(filename, 'r')
+file = open(filename, 'r', encoding="utf8")
 sql = " ".join(file.readlines())
 logger.info("Start executing: {} at {}".format(filename, str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))))
 cur.executescript(sql)
 
 logger.info("Import information of book")
 filename_title = 'databases/title.sql'
-file_title = open(filename_title, 'r')
+file_title = open(filename_title, 'r', encoding="utf8")
 sql_title = " ".join(file_title.readlines())
 logger.info("Start executing: {} at {}".format(filename_title, str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))))
 cur.executescript(sql_title)
 
 logger.info("Import barcode of book")
 filename_books = 'databases/books.sql'
-file_books = open(filename_books, 'r')
+file_books = open(filename_books, 'r', encoding="utf8")
 sql_books = " ".join(file_books.readlines())
 logger.info("Start executing: {} at {}".format(filename_books, str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))))
 cur.executescript(sql_books)
 
 logger.info("Import transaction")
 filename_transactions = 'databases/transactions.sql'
-file_transactions = open(filename_transactions, 'r')
+file_transactions = open(filename_transactions, 'r', encoding="utf8")
 sql_transactions = " ".join(file_transactions.readlines())
 logger.info(
     "Start executing: {} at {}".format(filename_transactions, str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))))
